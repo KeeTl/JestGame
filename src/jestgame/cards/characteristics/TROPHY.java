9580850318fb60f022c0495977a0b933c359c2be
KeeTl <kiran.kouroch@gmail.com>
2025-11-27 20:09:58 +0100

    ♠️♥️♣️♦️ Moved COLOR TROPHY and VALUE classes in subdirectory for better readability and organization

---
+++ src/jestgame/cards/characteristics/TROPHY.java
@@ -0,0 +1,11 @@
+package jestgame.cards.characteristics;
+
+public enum TROPHY {
+    HIGHEST,
+    LOWEST,
+    MAJORITY,
+    JOKER,
+    BESTJEST,
+    BESTJEST_NOJOKE
+    
+}
---
+++ src/jestgame/cards/characteristics/VALUE.java
@@ -0,0 +1,27 @@
+package jestgame.cards.characteristics;
+
+
+/**
+ * Value enum class, composed
+ */
+public enum VALUE{
+    ACE(1),
+    TWO(2),
+    THREE(3),
+    FOUR(4),
+    JOKER(6);
+
+    /**
+     * Not sure if I'll keep this like that
+     */
+    private final int v;
+
+    private VALUE(int v) {
+        this.v = v;
+    }
+
+    public int getV() {
+        return this.v;
+    }
+
+}
